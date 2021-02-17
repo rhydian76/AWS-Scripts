4c3e16286e8d7652f80ee5b2d9734268bcedcb96
rhydian76 <rhydian@drjenkins.org.uk>
2021-02-17 12:47:43 +0000

    Added ebs lifecycle policy creation script

---
+++ EBS Lifecycle Manager/configure-ebs-lifecycle-policy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set profile in the local aws credentials file for use below
+PROFILE="<profile from aws credentials file>"
+ACCOUNT_ID=$(aws sts get-caller-identity --profile $PROFILE |  jq -r '.Account')
+REGIONS=("eu-west-1" "eu-west-2" "us-east-1" "ap-northeast-1")
+ROLE_ARN="arn:aws:iam::$ACCOUNT_ID:role/AWSDataLifecycleManagerDefaultRole"
+
+aws dlm create-default-role --region $REGIONS \
+    --profile $PROFILE \
+
+for REGION in ${REGIONS[@]}
+do
+    aws dlm create-lifecycle-policy --state ENABLED \
+    --description "Creditsafe EBS Snapshot Policy 8 Day Retention" \
+    --execution-role-arn $ROLE_ARN \
+    --policy-details file://ebs-snapshot-8day.json \
+    --tags Project="Data Protection" \
+    --region $REGION --profile $PROFILE
+
+    aws dlm create-lifecycle-policy --state ENABLED \
+    --description "Creditsafe EBS Snapshot Policy 14 Day Retention" \
+    --execution-role-arn $ROLE_ARN \
+    --policy-details file://ebs-snapshot-14day.json \
+    --tags Project="Data Protection" \
+    --region $REGION --profile $PROFILE
+
+    aws dlm create-lifecycle-policy --state ENABLED \
+    --description "Creditsafe EBS Snapshot Policy 30 Day Retention" \
+    --execution-role-arn $ROLE_ARN \
+    --policy-details file://ebs-snapshot-30day.json \
+    --tags Project="Data Protection" \
+    --region $REGION --profile $PROFILE
+done
